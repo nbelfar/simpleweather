0431f6f3fa848c460c76cc10ed0dbc7431d46c17
Nora Belfar <nbelfar@gmail.com>
2014-03-08 23:01:07 -0500

    first commit

---
+++ js/weather.js
@@ -0,0 +1,32 @@
+angular.module('Weather', ['ngResource', 'ui.bootstrap']);
+
+function WeatherCtrl($scope, $resource) {
+	var latLng = {
+		lat: '',
+		lng: ''
+	};
+	var stringLoc = ''
+	$scope.locationLookupResult = "...";
+	$scope.locationLookup = function() {
+		
+	};
+
+	$scope.getWeather = function() {
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode( {address: $scope.searchZip}, function(result, status) {
+			if(status == google.maps.GeocoderStatus.OK){
+				  latLng.lat = result[0].geometry.location.lat();
+				  latLng.lng = result[0].geometry.location.lng();
+				  $scope.locationLookupResult = result[0].formatted_address;
+				  $scope.weatherLookupResult = $resource('https://api.forecast.io/forecast/f1199b0ddb52ea5a28158b1425d24d80/' + latLng.lat + ',' + latLng.lng,
+					{callback:'JSON_CALLBACK'},
+					{get:{method:'JSONP'}}).get();
+					var x = 'blah';
+			}
+			else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			}
+		});		
+	};
+
+}
